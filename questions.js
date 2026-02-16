d246d3b31352e8e87a07965db83ac227a31a1616
Trivia Help <115365701+triviahelp007@users.noreply.github.com>
2026-02-16 21:10:12 +0530

    Add questions array with arithmetic and reasoning questions

---
+++ questions.js
@@ -0,0 +1,17 @@
+let allQuestions = [
+
+{
+section: "Arithmetic",
+question: "20% of 600 = 25% of X. Find X.",
+options: ["460", "500", "480", "440"],
+answer: 2
+},
+
+{
+section: "Reasoning",
+question: "Laboratory : Germs",
+options: ["School : Students", "Playground : Games", "Library : Books", "Observatory : Planets"],
+answer: 3
+}
+
+];
